5b71b8c731f05fcf289c2843e5ebe5b7fbc7e37f
Donald Hays <donald.hays@gmail.com>
2018-06-13 09:35:30 -0700

    Setup work for formatting

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 
 import * as vscode from 'vscode';
 import { ASMHoverProvider } from "./hover";
-// import { ASMTypingFormatter } from "./formatter";
+// import { ASMFormatter, ASMTypingFormatter, ASMDocumentFormatter, ASMDocumentRangeFormatter } from "./formatter";
 import { ASMSymbolDocumenter } from "./symbolDocumenter";
 import { ASMCompletionProposer } from './completionProposer';
 import { ASMDefinitionProvider } from './definitionProvider';
@@ -11,9 +11,12 @@ import { ASMWorkspaceSymbolProvider } from './workspaceSymbolProvider';
 
 export function activate(context: vscode.ExtensionContext) {
   const symbolDocumenter = new ASMSymbolDocumenter();
+  // const formatter = new ASMFormatter();
 
   context.subscriptions.push(vscode.languages.registerHoverProvider({ language: "gbz80", scheme: "file" }, new ASMHoverProvider(symbolDocumenter)));
-  // context.subscriptions.push(vscode.languages.registerOnTypeFormattingEditProvider({ language: "gbz80", scheme: "file" }, new ASMTypingFormatter(), " ", ",", ";"));
+  // context.subscriptions.push(vscode.languages.registerOnTypeFormattingEditProvider({ language: "gbz80", scheme: "file" }, new ASMTypingFormatter(formatter), " ", ",", ";", ":"));
+  // context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider({ language: "gbz80", scheme: "file" }, new ASMDocumentFormatter(formatter)));
+  // context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider({ language: "gbz80", scheme: "file" }, new ASMDocumentRangeFormatter(formatter)));
   context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language: "gbz80", scheme: "file" }, new ASMCompletionProposer(symbolDocumenter)));
   context.subscriptions.push(vscode.languages.registerDefinitionProvider({ language: "gbz80", scheme: "file" }, new ASMDefinitionProvider(symbolDocumenter)));
   context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider({ language: "gbz80", scheme: "file" }, new ASMDocumentSymbolProvider(symbolDocumenter)));
--- src/formatter.ts
+++ src/formatter.ts
@@ -2,29 +2,54 @@
 
 import * as vscode from 'vscode';
 
-export class ASMTypingFormatter implements vscode.OnTypeFormattingEditProvider{
+export class ASMFormatter {
+  format(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions): vscode.ProviderResult<vscode.TextEdit[]> {
+    return null;
+  }
+}
+
+export class ASMDocumentFormatter implements vscode.DocumentFormattingEditProvider {
+  constructor(public formatter: ASMFormatter) { }
+  
+  provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+    return this.formatter.format(document, new vscode.Range(document.positionAt(0), document.positionAt(document.getText().length - 1)), options);
+  }
+}
+
+export class ASMDocumentRangeFormatter implements vscode.DocumentRangeFormattingEditProvider {
+  constructor(public formatter: ASMFormatter) { }
+  
+  provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+    return this.formatter.format(document, range, options);
+  }
+}
+
+export class ASMTypingFormatter implements vscode.OnTypeFormattingEditProvider {
+  constructor(public formatter: ASMFormatter) { }
+  
   public provideOnTypeFormattingEdits(document: vscode.TextDocument, position: vscode.Position, ch: string, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
-    const lineText = document.lineAt(position.line).text;
-    const lineRange = document.lineAt(position.line).range;
+    return this.formatter.format(document, document.lineAt(position.line).range, options);
+    // const lineText = document.lineAt(position.line).text;
+    // const lineRange = document.lineAt(position.line).range;
     
-    const commentIndentRegex = /^(.+?)(\s*)(;.*)$/;
-    const result = lineText.match(commentIndentRegex);
-    if (result) {
-      const pre = result[1];
-      const post = result[3];
-      if (pre.length < 40) {
-        let output = pre;
-        while (output.length < 40) {
-          output += " ";
-        }
-        output += post;
+    // const commentIndentRegex = /^(.+?)(\s*)(;.*)$/;
+    // const result = lineText.match(commentIndentRegex);
+    // if (result) {
+    //   const pre = result[1];
+    //   const post = result[3];
+    //   if (pre.length < 40) {
+    //     let output = pre;
+    //     while (output.length < 40) {
+    //       output += " ";
+    //     }
+    //     output += post;
         
-        return [new vscode.TextEdit(lineRange, output)];
-      } else {
-        return null;
-      }
-    } else {
-      return null;
-    }
+    //     return [new vscode.TextEdit(lineRange, output)];
+    //   } else {
+    //     return null;
+    //   }
+    // } else {
+    //   return null;
+    // }
   }
 }
